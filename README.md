7e05d9d40402a4e99a3543fb79badedbb5c8bb80
Mick Watkins <mick_watkins94@hotmail.com>
2016-12-26 12:24:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scrutiny App Prototype Website
+>Rapid prototype for Shaun Housman of Scrutiny Strength and Conditioning
+This is a website created for Scrutiny Strength & Conditioning as a prototype for a future mobile app. It will be used to determine the requirements and do initial testing before creating the application itself.
---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>TITLE</title>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        <div class="header row">
+            <div class="col-sm-12">
+                <h1>PAGE TITLE</h1>
+            </div>
+        </div>
+
+        <div class="content row">
+            <div class="content-pane-1 col-sm-4">
+            </div>
+
+            <div class="content-pane-2 col-sm-8">
+            </div>
+        </div>
+
+        <div class="footer row">
+            <div class="col-sm-12">
+                <h1>PAGE FOOTER</h1>
+            </div>
+        </div>
+
+    </body>
+    
+    <footer>
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjBf13Qu1XH0l-KcykGEM8LshQFw1c4Bc"></script>
+
+        <script type="text/javascript" src="js/functions.js"></script>
+    </footer>
+</html>
