9d79bb33159f36839161d318b09a254ed2a2c6f8
Mick Watkins <mick_watkins94@hotmail.com>
2017-01-07 08:52:42 +0000

    Fixed login page footer.

--- login.php
+++ login.php
@@ -15,6 +15,9 @@
 
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <!-- Bootstrap -->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     </head>
@@ -52,20 +55,18 @@
 
         </div>
 
-        <div class="footer row">
-            <div class="col-sm-12">
-                <h1>PAGE FOOTER</h1>
-            </div>
-        </div>
-
     </body>
     
     <footer>
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <div class="footer">
+            <div class="col-sm-12">
+                <div class="pull-right">
+                    <p>Scrutiny Strength & Conditioning</p>
+                    <p>Site designed and maintained by Mick Watkins</p>
+                </div>
+            </div>
+        </div>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjBf13Qu1XH0l-KcykGEM8LshQFw1c4Bc"></script>
-
         <script type="text/javascript" src="js/functions.js"></script>
         <script type="text/javascript" src="js/login.js"></script>
     </footer>
